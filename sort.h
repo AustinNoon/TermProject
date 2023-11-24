1bf60198933e5e89436a0bf13aa48aefb30bb38e
AustinNoon <123850831+AustinNoon@users.noreply.github.com>
2023-11-24 13:45:25 -0500

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,1 +1,18 @@
+#pragma once
+#ifndef SORT_H
+#define SORT_H
 
+#include <vector>
+#include <string>
+
+void insertionSort();
+void mergeSort();
+void quickSort();
+void bucketSort();
+
+void generateDataSet();
+
+template <typename T> 
+void benchmarkSort(void(*sortFunc)(T[], int), T arr[], int n, const std::string& algorithmName);
+
+#endif
