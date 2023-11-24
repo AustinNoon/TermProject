449e9227e34feeb872c1af26d1f146c019d10ff5
AustinNoon <123850831+AustinNoon@users.noreply.github.com>
2023-11-24 13:50:51 -0500

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -1,1 +1,17 @@
+#include <chrono>
 
+
+//function to benchmark the chosen sorting algorithm
+template <typename T>
+void benchmarkSort(void(*sortFunction)(T[], int), T arr[], int n, const std::string& algorithmName){
+    auto start = std::chrono::high_resolution_clock::now();
+
+    //call the sorting algorithm
+    sortFunction(arr, n);
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+    std::cout << "Algorithm: " << algorithmName << "\n";
+    std::cout << "Time taken by function: " << duration.count() << "microseconds\n\n";#
+}
