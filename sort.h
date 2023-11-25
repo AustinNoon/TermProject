b1ef06fb499776c1a672e2879c0d8dc630383e6c
joshel0 <150630356+joshel0@users.noreply.github.com>
2023-11-25 11:03:00 -0500

    Update sort.h

--- sort.h
+++ sort.h
@@ -5,14 +5,15 @@
 #include <vector>
 #include <string>
 
-void insertionSort();
-void mergeSort();
-void quickSort();
-void bucketSort();
+class Sort{
+public:
+  void insertionSort(std::vector<int>& arr);
+  void mergeSort(std::vector<int>& arr);
+  void quickSort(std::vector<int>& arr);
+  void bucketSort(std::vector<int>& arr, int range);
 
-void generateDataSet();
-
-template <typename T> 
-void benchmarkSort(void(*sortFunc)(T[], int), T arr[], int n, const std::string& algorithmName);
-
+  std::vector<int> getRanDataSet(const std::vector<int>& nums, int size);
+  void printList(const std::vector<int>& list);
+  void benchmarkSort(std::vector<int>& arr, const std::string& sortName, void (*sortFunction)(std::vector<int>&));
+}
 #endif
