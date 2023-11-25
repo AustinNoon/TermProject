0f4d8a4e025c39dbc94b229e6b85b9fb27629f72
joshel0 <150630356+joshel0@users.noreply.github.com>
2023-11-25 11:21:43 -0500

    Update sort.h

--- sort.h
+++ sort.h
@@ -12,8 +12,11 @@ public:
   void quickSort(std::vector<int>& arr);
   void bucketSort(std::vector<int>& arr, int range);
 
+class afterSort{
+public:
   std::vector<int> getRanDataSet(const std::vector<int>& nums, int size);
   void printList(const std::vector<int>& list);
   void benchmarkSort(std::vector<int>& arr, const std::string& sortName, void (*sortFunction)(std::vector<int>&));
+
 }
 #endif
