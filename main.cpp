2e600c02f231755331137ea41758580574b53391
joshel0 <150630356+joshel0@users.noreply.github.com>
2023-11-25 11:14:44 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,19 @@
 #include <sstream>
 #include <iostream>
 
+std::vector<int> getNumbers(const std::string& fname){
+    std::vector<int> num;
+    std::ifstream infile(fname);
+
+    int x;
+    while(infile >> x){
+        num.push_back(x);
+    }
+    
+    infile.close();
+    return num;
+}
+
 int main(int argc, char*argv[]){
     std::ifstream ifs(argv[1]);
    
