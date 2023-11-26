3c300374f4be4aaac3abba612b28888566d54b3e
joshel0 <150630356+joshel0@users.noreply.github.com>
2023-11-26 11:39:05 -0500

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,4 +1,3 @@
-#pragma once
 #ifndef SORT_H
 #define SORT_H
 
@@ -7,16 +6,8 @@
 
 class Sort{
 public:
-  void insertionSort(std::vector<int>& arr);
-  void mergeSort(std::vector<int>& arr);
-  void quickSort(std::vector<int>& arr);
-  void bucketSort(std::vector<int>& arr, int range);
-  std::vector<int> getRanDataSet(const std::vector<int>& nums, int size);
+    static std::vector<int> getNumbers(const std::string& filename, int count);
+    static void printList(const std::vector<int>& list);
+};
 
-class afterSort{
-public:
-  void printList(const std::vector<int>& list);
-  void benchmarkSort(std::vector<int>& arr, const std::string& sortName, void (*sortFunction)(std::vector<int>&));
-
-}
-#endif
+#endif 
