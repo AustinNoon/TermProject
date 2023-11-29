b5ff489e3748fa10f823a822578dfd7bb0e72a96
joshel0 <150630356+joshel0@users.noreply.github.com>
2023-11-29 15:06:09 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,30 @@ int main(int argc, char* argv[]){
     Sort::printList(ranNumbers);
     std::cout << std::endl;
 
+    std::vector<int> insertionSortNumbers = ranNumbers;
+    Sort::insertionSort(ranNumbers);
+    std::cout << "Insertion Sort: ";
+    Sort::printList(ranNumbers);
+    Sort::runTime(Sort::insertionSort, insertionSortNumbers);
+    std::cout << std::endl;
+
+    std::vector<int> ranNumbers2 = Sort::getNumbers(filename);
+    
+    std::vector<int> quickSortNumbers = ranNumbers2;
+    Sort::quickSort(quickSortNumbers);
+    std::cout << "Quick Sort: ";
+    Sort::printList(quickSortNumbers);
+    Sort::runTime(Sort::quickSort, quickSortNumbers);
+    std::cout << std::endl;
+
+    std::vector<int> ranNumbers3 = Sort::getNumbers(filename);
+    
+    std::vector<int> mergeSortNumbers = ranNumbers3;
+    Sort::mergeSort(mergeSortNumbers);
+    std::cout << "Merge Sort: ";
+    Sort::printList(mergeSortNumbers);
+    Sort::runTime(Sort::mergeSort, mergeSortNumbers);
+    std::cout << std::endl;
 
     return 0;
 }
