2ba5b0360018bce072ce07158e4e4f4294d8c954
joshel0 <150630356+joshel0@users.noreply.github.com>
2023-11-29 15:38:57 -0500

    Update sort.cpp
    added a space

--- sort.cpp
+++ sort.cpp
@@ -137,7 +137,7 @@ void Sort::runTime(void(*sortFunction)(std::vector<int>&), std::vector<int>& lis
     auto stop = std::chrono::high_resolution_clock::now(); // point to stop the clock
 
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count(); //finding the duration of algorithm by subtracting stop from start
-    std::cout << "Time taken by: " << std::fixed << std::setprecision(6) << duration << "microseconds" << std::endl; //print the time taken to run function
+    std::cout << "Time taken by: " << std::fixed << std::setprecision(6) << duration << " microseconds" << std::endl; //print the time taken to run function
 }
 
 void Sort::printList(const std::vector<int>& list){
