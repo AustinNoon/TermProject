21e451faba3ccd76682a411532e917a536586325
joshel0 <150630356+joshel0@users.noreply.github.com>
2023-11-29 10:35:15 -0500

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -4,32 +4,23 @@
 #include <sstream>
 #include <cstdlib>
 #include <ctime>
+#include <chrono>
+#include <iomanip>
 
-std::vector<int> Sort::getNumbers(const std::string& filename, int count){
+std::vector<int> Sort::getNumbers(const std::string& filename){
     std::vector<int> numbers;
 
     std::ifstream inputFile(filename);
 
-    std::string line;
-    while(std::getline(inputFile, line)){
-        std::istringstream iss(line);
-        int num;
-        while(iss >> num){
-            numbers.push_back(num);
-        }
+    int num;
+    while(inputFile >> num){
+        numbers.push_back(num);
     }
 
-    srand(static_cast<unsigned int>(time(nullptr)));
-
-    std::vector<int> ranNum;
-    for(int i = 0; i < count && !numbers.empty(); ++i){
-        int index = rand() % numbers.size();
-        ranNum.push_back(numbers[index]);
-        numbers.erase(numbers.begin() + index);
-    }
-
-    return ranNum;
+    return numbers;
 }
+   
+
 
 void Sort::printList(const std::vector<int>& list){
     for(int num : list){
