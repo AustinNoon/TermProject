e4a8cbdd410b8d7369026942bfba96b2fdc83d86
joshel0 <150630356+joshel0@users.noreply.github.com>
2023-11-29 15:34:15 -0500

    Update main.cpp
    changes variable names

--- main.cpp
+++ main.cpp
@@ -3,31 +3,30 @@
 
 int main(int argc, char* argv[]){
     std::string filename = argv[1];
-    std::vector<int> ranNumbers = Sort::getNumbers(filename);
+    std::vector<int> data = Sort::getData(filename);//gets data from file
 
-    std::cout << "Random Numbers: ";
-    Sort::printList(ranNumbers);
+    std::cout << "Numbers: ";
+    Sort::printList(data);
     std::cout << std::endl;
 
-    std::vector<int> insertionSortNumbers = ranNumbers;
-    Sort::insertionSort(ranNumbers);
+    std::vector<int> insertionSortNumbers = data;//gets original data
+    Sort::insertionSort(data);
     std::cout << "Insertion Sort: ";
-    Sort::printList(ranNumbers);
+    Sort::printList(data);
     Sort::runTime(Sort::insertionSort, insertionSortNumbers);
     std::cout << std::endl;
 
-    std::vector<int> ranNumbers2 = Sort::getNumbers(filename);
-    
-    std::vector<int> quickSortNumbers = ranNumbers2;
+    std::vector<int> quickData = Sort::getData(filename);//gets original data
+    std::vector<int> quickSortNumbers = quickData;
     Sort::quickSort(quickSortNumbers);
     std::cout << "Quick Sort: ";
     Sort::printList(quickSortNumbers);
     Sort::runTime(Sort::quickSort, quickSortNumbers);
     std::cout << std::endl;
 
-    std::vector<int> ranNumbers3 = Sort::getNumbers(filename);
+    std::vector<int> mergeData = Sort::getData(filename);//gets original data
     
-    std::vector<int> mergeSortNumbers = ranNumbers3;
+    std::vector<int> mergeSortNumbers = mergeData;
     Sort::mergeSort(mergeSortNumbers);
     std::cout << "Merge Sort: ";
     Sort::printList(mergeSortNumbers);
