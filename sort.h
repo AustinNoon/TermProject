a263f32ed20e06f76b812d8ccfb7a261b600c356
matthewcarroll2020 <113484433+matthewcarroll2020@users.noreply.github.com>
2023-11-29 16:53:24 -0500

    Update sort.h
    add saveSortedToFile method

--- sort.h
+++ sort.h
@@ -9,16 +9,18 @@ class Sort{
 public:
     static std::vector<int> getData(const std::string& filename);
     static void printList(const std::vector<int>& list);
+    static void saveSortedToFile(const std::vector<int>& list, std::string outName);
     static void insertionSort(std::vector<int>& list);
     static void quickSort(std::vector<int>& list);
     static void mergeSort(std::vector<int>& list);
-    static void bucketSort(std::vector<int>& list);
+    static void bucketSort();
     static void runTime(void(*sortFunction)(std::vector<int>&), std::vector<int>& list);
-private: 
+private:
     static void quickHelper(std::vector<int>& list, int low, int high);
     static int partition(std::vector<int>& list, int low, int high);
     static void merge(std::vector<int>& list, int left, int mid, int right);
     static void mergeHelper(std::vector<int>& list, int left, int right);
 };
 
-#endif 
+#endif
+
